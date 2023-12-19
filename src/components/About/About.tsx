6a720e31cbc5738ca4213e3bdf6d5ce65c36fc3c
Pablo Medina <jmedinamulet@gmail.com>
2023-12-19 15:09:47 -0500

    Add About component to Home page

---
+++ src/components/About/About.tsx
@@ -0,0 +1,14 @@
+export function About() {
+    return (
+        <section className="p-8 flex">
+            <svg width="2" height="116" viewBox="0 0 2 116" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path opacity="0.3" d="M1 0V116" stroke="white" strokeDasharray="3 3"/>
+            </svg>
+            <article className="font-inter text-sm pl-4 w-full">
+                <p className="mb-4">I'm Pablo Medina, a Software Engineer based in New York. </p>
+                <p className="mb-4">Being a first generation immigrant, I transitioned from being a medical first responder to software engineering. </p>
+                <p>Recently I got a chance to work on projects spanning WEB 3 marketplace technologies to full-stack applications used for such and such.</p>
+            </article>
+        </section>
+    )
+}
