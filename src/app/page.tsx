dad3f4d01be4a8bf9df5e205157c3a448b765a52
Pablo Medina <jmedinamulet@gmail.com>
2023-12-28 11:36:31 -0500

    Add Experience and MyApps components to Home page, update Header styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { About } from '@/components/About/About'
 import { Header } from '../components/Header/Header'
+import { Experience } from '@/components/Experience/Experience'
+import { MyApps } from '@/components/MyApps/MyApps'
 
 export default function Home() {
     return (
         <main>
           <Header />
           <About />
+          <Experience />
+          <MyApps />
         </main>
     )
 }
