559a893f8e97f8855b5caf9e6892d89b9bd511d2
Pablo Medina <jmedinamulet@gmail.com>
2023-12-31 12:58:06 -0500

    Refactor GitCal component layout and add language and technology lists

--- src/components/GitCal/GitCal.tsx
+++ src/components/GitCal/GitCal.tsx
@@ -38,12 +38,37 @@ export function GitCal() {
 
 
     return (
-        // p-4 md:p-8 flex justify-center items-center text-white font-inter m-auto
-        <section className="p-4 md:p-8 flex items-center justify-between text-white font-inter m-auto">
+        <section className="p-4 md:p-8 text-white font-inter mx-auto flex">
             <svg width="2" height="160" viewBox="0 0 2 160" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path opacity="0.3" d="M1 0V160" stroke="white" strokeDasharray="3 3"/>
             </svg>
-                <div className='rounded-xl border border-[#3D3B3E] p-4'>
+            <section className='flex justify-between items-start '>
+                <article className='font-inter text-sm md:text-base ml-4 md:ml-6 basis-2/4'>
+                    <h2 className='text-[#B7B7B7] mb-4 md:text-lg'>Languages I work with:</h2>
+                        <ul className='list-none grid grid-cols-2 gap-x-2 gap-y-1'>
+                            <li>JavaScript</li>
+                            <li>TypeScript</li>
+                            <li>Python</li>
+                            <li>Go</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>C#</li>
+                        </ul>
+                </article>
+                <article className='font-inter text-sm md:text-base ml-4 md:ml-6 basis-2/4 mr-2'>
+                    <h2 className='text-[#B7B7B7] mb-4 md:text-lg'>Frameworks and other Technologies:</h2>
+                    <ul className='list-none grid grid-cols-2 gap-x-2 gap-y-1'>
+                        <li>React</li>
+                        <li>Next.js</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>FastApi</li>
+                        <li>Sass</li>
+                        <li>Tailwind</li>
+                    </ul>
+                </article>
+            </section>
+                <div className='rounded-xl border border-[#3D3B3E] p-4 w-2/4'>
                     <GitHubCalendar
                         username="jmedina21"
                         blockSize={10}
