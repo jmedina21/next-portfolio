599fffe1ee0b2663e2c5fca0605b95541b971b50
Pablo Medina <jmedinamulet@gmail.com>
2024-01-01 00:26:08 -0500

    Update GitCal component styling and language and technology lists

--- src/components/GitCal/GitCal.tsx
+++ src/components/GitCal/GitCal.tsx
@@ -1,5 +1,8 @@
 'use client'
+import './GitCal.scss';
+import Image from 'next/image';
 import GitHubCalendar from 'react-github-calendar';
+import stich from '../../assets/stich.svg'
 
 interface Contribution{
     date: string,
@@ -7,82 +10,84 @@ interface Contribution{
     level: 0 | 1 | 2 | 3 | 4
 }
 
-interface Activity {
-    date: string;
-    count: number;
-    level: 0 | 1 | 2 | 3 | 4;
-  }
-
 export function GitCal() {
 
     const colorTheme = {
-        dark: ['#1B1B1B','#3C3C3C','#5F5F5F', '#929292','#BABABA'],
+        dark: ['#1B1B1B','#BABABA'],
       };
 
-      const selectLastHalfYear = (contributions: Contribution[]) => {
-        const currentYear = new Date().getFullYear();
-        const currentMonth = new Date().getMonth();
+      function selectLastHalfYear(contributions: Contribution[]) {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = currentDate.getMonth();
         const shownMonths = 6;
-      
-        return contributions.filter((contribution: Contribution) => {
+    
+        return contributions.filter((contribution) => {
             const date = new Date(contribution.date);
-            const monthOfDay = date.getMonth();
-            
-            return (
-                date.getFullYear() === currentYear &&
-                monthOfDay > currentMonth - shownMonths &&
-                monthOfDay <= currentMonth
-            );
-          });
-      };
-
+            const year = date.getFullYear();
+            const month = date.getMonth();
+    
+            if (currentMonth >= shownMonths) {
+                return year === currentYear && month > currentMonth - shownMonths && month <= currentMonth;
+            } else {
+                return (
+                    (year === currentYear && month <= currentMonth) ||
+                    (year === currentYear - 1 && month > 11 - (shownMonths - currentMonth))
+                );
+            }
+        });
+    };
+    
 
     return (
         <section className="p-4 md:p-8 text-white font-inter mx-auto flex">
-            <svg width="2" height="160" viewBox="0 0 2 160" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path opacity="0.3" d="M1 0V160" stroke="white" strokeDasharray="3 3"/>
-            </svg>
-            <section className='flex justify-between items-start '>
-                <article className='font-inter text-sm md:text-base ml-4 md:ml-6 basis-2/4'>
-                    <h2 className='text-[#B7B7B7] mb-4 md:text-lg'>Languages I work with:</h2>
+            <Image src={stich} alt='decorative-line'/>
+            <article className='flex flex-col md:flex-row gap-4'>
+                <div className='flex flex-col ml-4 md:flex-row md:ml-6 gap-4'>
+                    <div className='font-inter text-sm md:text-base md:basis-2/4' >
+                        <h2 className='text-[#B7B7B7] mb-2 md:text-lg'>Languages I work with:</h2>
+                            <ul className='list-none grid grid-cols-2 gap-x-2 gap-y-1'>
+                                <li>JavaScript</li>
+                                <li>Python</li>
+                                <li>TypeScript</li>
+                                <li>Go</li>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>C#</li>
+                            </ul>
+                    </div>
+                    <div className='font-inter text-sm md:text-base md:basis-2/4' >
+                        <h2 className='text-[#B7B7B7] mb-2 md:text-lg'>Frameworks and other Technologies:</h2>
                         <ul className='list-none grid grid-cols-2 gap-x-2 gap-y-1'>
-                            <li>JavaScript</li>
-                            <li>TypeScript</li>
-                            <li>Python</li>
-                            <li>Go</li>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>C#</li>
+                            <li>React</li>
+                            <li>Next.js</li>
+                            <li>Node</li>
+                            <li>Express</li>
+                            <li>FastApi</li>
+                            <li>Sass</li>
+                            <li>Tailwind</li>
+                            <li>Jest</li>
                         </ul>
-                </article>
-                <article className='font-inter text-sm md:text-base ml-4 md:ml-6 basis-2/4 mr-2'>
-                    <h2 className='text-[#B7B7B7] mb-4 md:text-lg'>Frameworks and other Technologies:</h2>
-                    <ul className='list-none grid grid-cols-2 gap-x-2 gap-y-1'>
-                        <li>React</li>
-                        <li>Next.js</li>
-                        <li>Node</li>
-                        <li>Express</li>
-                        <li>FastApi</li>
-                        <li>Sass</li>
-                        <li>Tailwind</li>
-                    </ul>
-                </article>
-            </section>
-                <div className='rounded-xl border border-[#3D3B3E] p-4 w-2/4'>
-                    <GitHubCalendar
-                        username="jmedina21"
-                        blockSize={10}
-                        blockMargin={3}
-                        theme={colorTheme}
-                        fontSize={14}
-                        colorScheme="dark"
-                        transformData={selectLastHalfYear}
-                        transformTotalCount={false}
-                        labels={{
-                            totalCount: '{{count}} contributions in the last 6 months',
-                        }}
-                    />
+                    </div>
                 </div>
+                <GitHubCalendar
+                    username="jmedina21"
+                    blockSize={10}
+                    blockMargin={3}
+                    theme={colorTheme}
+                    fontSize={14}
+                    colorScheme="dark"
+                    transformData={selectLastHalfYear}
+                    labels={{
+                        totalCount: '{{count}} contributions in the last 6 months',
+                    }}
+                    eventHandlers={{
+                        onClick: (event) => (activity) => {
+                          alert(JSON.stringify(activity));
+                        },
+                    }}
+                />
+            </article>
         </section>
     )
 }
