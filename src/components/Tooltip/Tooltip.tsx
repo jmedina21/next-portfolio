93f5beb1bfe9e424993696e626246e8f9b813eb5
Pablo Medina <jmedinamulet@gmail.com>
2024-01-05 10:58:29 -0500

    Refactor Tooltip styling and position

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -7,11 +7,10 @@ interface TooltipProps {
 }
 
 export function Tooltip({x, y, content}: TooltipProps) {
-    console.log(x, y, content)
     
     return (
         <div className="tooltip bg-[#6F7680] text-white text-xs absolute"
-            style={{ left: `${x}px`, top: `${y}px`, transform: 'translate(-50%, -50%)' }}
+            style={{ left: `${x}px`, top: `${y - 17}px`, transform: 'translate(-50%, -50%)' }}
         >
             <p>{content}.</p>
         </div>
